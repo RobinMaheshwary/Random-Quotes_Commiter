0e714103754ff981aef31232c89c7483f27d94d9
Robin Maheshwary <robinmaheshwari1234567@gmail.com>
2023-12-03 17:23:56 +0530

    Qoute added

--- commiter.py
+++ commiter.py
@@ -9,16 +9,19 @@ def commit_changes(message):
     subprocess.run(['git', 'add', '.'])
     subprocess.run(['git', 'commit', '-m', message])
 
-# Function to push changes
-def push_changes():
+# Function to push changes and sync
+def push_and_sync():
     subprocess.run(['git', 'push'])
+    subprocess.run(['git', 'fetch', 'origin'])
+    subprocess.run(['git', 'merge', 'origin/main'])  # Replace "main" with your branch name if different
+
 
 # Example: Auto commit every hour
 while True:
     commit_message = message
     commit_changes(commit_message)
-    push_changes()
-    print("Changes committed and pushed to the repository.")
-    
+    push_and_sync()
+    print("Changes committed, pushed, and synchronized with the repository.")
+
     # Wait for an hour before the next commit
     time.sleep(20)  # 3600 seconds = 1 hour
